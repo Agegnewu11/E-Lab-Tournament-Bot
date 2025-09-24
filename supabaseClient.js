175a456271a834dcf1dca50722e18ea18524d604
Agegnewu11 <agegnewu3@gmail.com>
2025-09-24 16:44:47 +0300

    Create supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://ucciujxlmkfcyqrcwueu.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjY2l1anhsbWtmY3lxcmN3dWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MDE3MDAsImV4cCI6MjA3NDI3NzcwMH0.U1Xe7VpClwY2m5YljE08Tr0KZDSL-6Br5qS6Mz0EvCI"
+);
